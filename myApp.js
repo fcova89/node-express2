5c6305ae5a364d0ad46997a4f299571b7e79762a
Cist1 <fcova89@gmail.com>
2022-03-04 07:19:30 +0100

    Aggiunto path & index

--- myApp.js
+++ myApp.js
@@ -1,5 +1,12 @@
 var express = require('express');
+const path = require('path');
+
 var app = express();
+
+app.get("/",function(req, res) {
+  res.sendFile(path.join(__dirname, '/views/index.html'));
+    //res.sendFile(__dirname/views/index.html);
+})
 
 
 
