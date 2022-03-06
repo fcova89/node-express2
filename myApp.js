a992c3e284f36f19bcc809b64bd3eae2fe1a3453
Cist1 <fcova89@gmail.com>
2022-03-06 18:00:08 +0100

    test body parser2

--- myApp.js
+++ myApp.js
@@ -7,6 +7,7 @@ var app = express();
 var jsonObj = {message: "Hello json"}; 
 
 app.use('/public',express.static(path.join(__dirname, '/public'))); //middleware per style.css
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(function (req,res,next) {
   console.log(`${req.method} ${req.path} - ${req.ip}`); //test loggo il method recupero method
